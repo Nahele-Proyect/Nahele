c42b97601196691bc750c07b3862bc2eb4976285
nelson1812 <nelsoncc1812@gmail.com>
2020-03-02 16:38:11 +0100

    services scrap done

---
+++ cliente/src/services/scrap.service.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export default class Services {
+    constructor() {
+        this.service = new axios.create({
+            baseURL: "http://localhost:5000/api/scrap",
+            withCredentials: true
+        })
+    }
+    getAll = () => this.service.get('/').then(response => response.data)
+    getDetails = endpoint => this.service.get('/details' + endpoint).then(response => response.data)
+}
