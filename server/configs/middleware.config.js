90083821e1785f1cb2726f489a7ed4e0cc877ced
nelson1812 <nelsoncc1812@gmail.com>
2020-03-05 15:35:44 +0100

    sdf

--- server/configs/middleware.config.js
+++ server/configs/middleware.config.js
@@ -24,7 +24,7 @@ module.exports = app => {
     sourceMap: true
   }))
 
-  const whitelist = ['https://nahele-proyect.herokuapp.com']
+  const whitelist = ['http://localhost:3000']
   const corsOptions = {
     origin: (origin, cb) => {
       const originWhitelisted = whitelist.includes(origin)
