1323454f39199ad8767cca5d1c35d21cf1b2f0a5
Andres Atehortua <anderson17colombia@hotmail.com>
2020-03-09 18:59:02 +0100

    cambios mamalones

--- client/src/components/pages/Profile/PetsList/IndexPetListCard/IndexPetListCard.js
+++ client/src/components/pages/Profile/PetsList/IndexPetListCard/IndexPetListCard.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const IndexPetListCard = props => {
-    console.log(props)
+
     return (
-        <div className='idexPetListCard'><Link to={ '/myPet/' + props._id }>{ props.name }</Link></div>
+        <div className='idexPetListCard'><Link to={'/myPet/' + props._id}>{props.name}</Link></div>
     )
 }
 
