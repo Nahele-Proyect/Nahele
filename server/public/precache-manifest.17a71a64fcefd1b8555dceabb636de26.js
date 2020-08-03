6955ebb9ca9d8a9beb41babb86713a00fe7f97d7
Andres Atehortua <anderson17colombia@hotmail.com>
2020-08-03 20:08:19 +0200

    deploy

---
+++ server/public/precache-manifest.17a71a64fcefd1b8555dceabb636de26.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb670d1140a9c7c5f2c25283cd49c55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "254d6d4e0a4f1fceeb51",
+    "url": "/static/css/3.5826eb4e.chunk.css"
+  },
+  {
+    "revision": "c2889259754945394d96",
+    "url": "/static/css/main.4be93c28.chunk.css"
+  },
+  {
+    "revision": "254d6d4e0a4f1fceeb51",
+    "url": "/static/js/3.255e4365.chunk.js"
+  },
+  {
+    "revision": "f1b0a9fde5b7d9d86cc20d1e868e9cde",
+    "url": "/static/js/3.255e4365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b04ec24951fbeb0e0a",
+    "url": "/static/js/4.60420ac5.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/static/js/4.60420ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9f7a03ce5d2e05c2a0",
+    "url": "/static/js/5.542513c8.chunk.js"
+  },
+  {
+    "revision": "6360695b40e1dbae650c",
+    "url": "/static/js/6.cbe17c9c.chunk.js"
+  },
+  {
+    "revision": "c2889259754945394d96",
+    "url": "/static/js/main.048b046d.chunk.js"
+  },
+  {
+    "revision": "64311f2958f2bf944180",
+    "url": "/static/js/runtime-main.9d73e022.js"
+  },
+  {
+    "revision": "274c7bb810fb7258ada2",
+    "url": "/static/js/xlsx.b1ee8eab.chunk.js"
+  },
+  {
+    "revision": "cc4457c09205ab394fda90d4ae73ad4c",
+    "url": "/static/media/background.cc4457c0.png"
+  },
+  {
+    "revision": "b25e5056cc906cf49f1e5e3a490eca5a",
+    "url": "/static/media/email.b25e5056.svg"
+  },
+  {
+    "revision": "e03cdebed2d3a31334da81e627a6fb86",
+    "url": "/static/media/jake.e03cdebe.png"
+  },
+  {
+    "revision": "61c7aa614d5febd3ee43456807a34921",
+    "url": "/static/media/location.61c7aa61.svg"
+  },
+  {
+    "revision": "cee4ba084bc411a5f2402ba071ab4a13",
+    "url": "/static/media/phone.cee4ba08.svg"
+  },
+  {
+    "revision": "3943348dc3a11719818683fceebe505a",
+    "url": "/static/media/suited-dog.3943348d.jpg"
+  }
+]);
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89b3acded85b7b439ed6491e324a6bf9.js"
+  "/precache-manifest.17a71a64fcefd1b8555dceabb636de26.js"
 );
 
 self.addEventListener('message', (event) => {
