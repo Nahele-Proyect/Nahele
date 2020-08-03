633db19e36b4a38de092ed75ea87803c5cbf1a8d
Andres Atehortua <anderson17colombia@hotmail.com>
2020-08-03 20:04:36 +0200

    Cambios necesarios

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,151 @@
 //Reac imports
-import React, { Component } from 'react'
-import { Switch, Route, Redirect } from "react-router-dom"
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 //Services
-import AuthServices from './services/auth.service'
+import AuthServices from "./services/auth.service";
 //Self-made imports
-import Navbar from './components/ui/Navbar/Navbar'
-import Footer from './components/ui/Footer/Footer'
-import Profile from './components/pages/Profile/Profile'
-import Index from './components/pages/Index/index'
-import Details from './components/pages/Details/Details'
-import About from './components/pages/About/About'
+import Navbar from "./components/ui/Navbar/Navbar";
+import Footer from "./components/ui/Footer/Footer";
+import Profile from "./components/pages/Profile/Profile";
+import Index from "./components/pages/Index/index";
+import Details from "./components/pages/Details/Details";
+import About from "./components/pages/About/About";
 
-import Chat from './components/ChatComponent/Chat/Chat'
-import Join from './components/ChatComponent/Join/Join'
-import Map from './components/GoogleMaps/GoogleMaps'
-import Calendar from './components/Calendar/Calendar'
+import Chat from "./components/ChatComponent/Chat/Chat";
+import Join from "./components/ChatComponent/Join/Join";
+import Map from "./components/GoogleMaps/GoogleMaps";
+import Calendar from "./components/Calendar/Calendar";
 //Spinner imports
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
-import Loader from 'react-loader-spinner'
-
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
-      loggedInUser: undefined
-    }
-    this.AuthServices = new AuthServices()
+      loggedInUser: undefined,
+    };
+    this.AuthServices = new AuthServices();
   }
 
-  componentDidMount = () => this.fetchUser()
+  componentDidMount = () => this.fetchUser();
 
-  setTheUser = userObj => this.setState({ loggedInUser: userObj })
+  setTheUser = (userObj) => this.setState({ loggedInUser: userObj });
 
   fetchUser = () => {
     this.AuthServices.loggedin()
-      .then(theUser => theUser.status === 'fail' ? this.setState({ loggedInUser: false }) : this.setState({ loggedInUser: theUser }))
-      .catch(() => this.setState({ loggedInUser: false }))
-  }
+      .then((theUser) =>
+        theUser.status === "fail"
+          ? this.setState({ loggedInUser: false })
+          : this.setState({ loggedInUser: theUser })
+      )
+      .catch(() => this.setState({ loggedInUser: false }));
+  };
 
   render() {
-
     return (
       <>
-        {this.state.loggedInUser !== undefined ?
+        {this.state.loggedInUser !== undefined ? (
           <>
-            <Navbar setTheUser={this.setTheUser} loggedInUser={this.state.loggedInUser} />
+            <Navbar
+              setTheUser={this.setTheUser}
+              loggedInUser={this.state.loggedInUser}
+            />
 
             <Switch>
-              <Route exact path='/' render={() => <Index  {...this.state.loggedInUser} />} />
-              <Route path='/details/:link' render={props => <Details loggedInUser={this.state.loggedInUser} setTheUser={this.setTheUser} {...props} />} />
-              <Route path='/myPet/:id' render={props => <Details loggedInUser={this.state.loggedInUser} setTheUser={this.setTheUser} {...props} />} />
-              <Route path="/profile" render={() => this.state.loggedInUser ? <Profile loggedInUser={this.state.loggedInUser} setTheUser={this.setTheUser} /> : <Redirect to="/" />} />
-              <Route path='/about' render={() => <About />} />
+              <Route
+                exact
+                path="/"
+                render={() => <Index {...this.state.loggedInUser} />}
+              />
+              <Route
+                path="/details/:link"
+                render={(props) => (
+                  <Details
+                    loggedInUser={this.state.loggedInUser}
+                    setTheUser={this.setTheUser}
+                    {...props}
+                  />
+                )}
+              />
+              <Route
+                path="/myPet/:id"
+                render={(props) => (
+                  <Details
+                    loggedInUser={this.state.loggedInUser}
+                    setTheUser={this.setTheUser}
+                    {...props}
+                  />
+                )}
+              />
+              <Route
+                path="/profile"
+                render={() =>
+                  this.state.loggedInUser ? (
+                    <Profile
+                      loggedInUser={this.state.loggedInUser}
+                      setTheUser={this.setTheUser}
+                    />
+                  ) : (
+                    <Redirect to="/" />
+                  )
+                }
+              />
+              <Route path="/about" render={() => <About />} />
 
-              <Route path="/join" render={() => this.state.loggedInUser ? <Join loggedInUser={this.state.loggedInUser} /> : <Redirect to='/' />} />
-              <Route path="/chat" render={props => this.state.loggedInUser ? <Chat {...props} loggedInUser={this.state.loggedInUser} /> : <Redirect to='/' />} />
-              <Route path="/map" render={props => <Map {...props} />} />
-              <Route path="/newCalendar/:id" render={props => <Calendar {...props} loggedInUser={this.state.loggedInUser} fetchUser={this.fetchUser} />} />
-
+              <Route
+                path="/join"
+                render={() =>
+                  this.state.loggedInUser ? (
+                    <Join loggedInUser={this.state.loggedInUser} />
+                  ) : (
+                    <Redirect to="/" />
+                  )
+                }
+              />
+              <Route
+                path="/chat"
+                render={(props) =>
+                  this.state.loggedInUser ? (
+                    <Chat {...props} loggedInUser={this.state.loggedInUser} />
+                  ) : (
+                    <Redirect to="/" />
+                  )
+                }
+              />
+              <Route path="/map" render={(props) => <Map {...props} />} />
+              <Route
+                path="/newCalendar/:id"
+                render={(props) => (
+                  <Calendar
+                    {...props}
+                    loggedInUser={this.state.loggedInUser}
+                    fetchUser={this.fetchUser}
+                  />
+                )}
+              />
             </Switch>
             <Footer />
           </>
-          :
-          <Loader style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }} type="Rings" color="red" height={250} width={250} timeout={10000} />
-        }
-
+        ) : (
+          <Loader
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              minHeight: "80vh",
+            }}
+            type="Rings"
+            color="red"
+            height={250}
+            width={250}
+            timeout={10000}
+          />
+        )}
       </>
-    )
+    );
   }
 }
 
-
-export default App
+export default App;
--- server/configs/middleware.config.js
+++ server/configs/middleware.config.js
@@ -1,40 +1,37 @@
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const favicon = require('serve-favicon')
-const logger = require('morgan')
-const path = require('path')
-const express = require('express')
-const cors = require('cors')
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
 
-module.exports = app => {
+module.exports = (app) => {
+  const app_name = require("../package.json").name;
+  const debug = require("debug")(
+    `${app_name}:${path.basename(__filename).split(".")[0]}`
+  );
 
-  const app_name = require('../package.json').name
-  const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`)
+  app.use(logger("dev"));
+  app.use(bodyParser.json());
+  app.use(
+    bodyParser.urlencoded({
+      extended: false,
+    })
+  );
+  app.use(cookieParser());
 
-  app.use(logger('dev'))
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({
-    extended: false
-  }))
-  app.use(cookieParser())
-
-  app.use(require('node-sass-middleware')({
-    src: path.join(__dirname, '..', 'public'),
-    dest: path.join(__dirname, '..', 'public'),
-    sourceMap: true
-  }))
-
-  const whitelist = ['http://localhost:3000']
+  const whitelist = ["http://localhost:3000"];
   const corsOptions = {
     origin: (origin, cb) => {
-      const originWhitelisted = whitelist.includes(origin)
-      cb(null, originWhitelisted)
+      const originWhitelisted = whitelist.includes(origin);
+      cb(null, originWhitelisted);
     },
-    credentials: true // RUTAS PERSISTENTES
-  }
+    credentials: true, // RUTAS PERSISTENTES
+  };
 
-  app.use(cors(corsOptions))
-  app.set('views', path.join(__dirname, '..', 'views'))
-  app.set('view engine', 'hbs')
-  app.use(express.static(path.join(__dirname, '..', 'public')))
-}
+  app.use(cors(corsOptions));
+  app.set("views", path.join(__dirname, "..", "views"));
+  app.set("view engine", "hbs");
+  app.use(express.static(path.join(__dirname, "..", "public")));
+};
